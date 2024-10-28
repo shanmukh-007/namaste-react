b6e1037fb5588820ce9f2b327004d9a663696f6c
Shanmukha Srinivasu <chitturi.srinivasu@accenture.com>
2024-10-28 12:28:45 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1",{id : "heading", xyz :"abc"},"Hello World by React");
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
